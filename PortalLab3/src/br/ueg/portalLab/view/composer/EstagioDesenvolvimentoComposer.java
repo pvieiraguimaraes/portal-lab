646615bf2f6b9e0aeda79618ce4c587bc3e87901
Guiliano <Guiliano@cac3254f-852a-ee89-7979-6c55c4adcd7b>
2012-04-04 05:51:10 +0000

    Completado uma etapa do desenvolvimento dos casos de crud simples. Casos de uso implementados: Tipo de montagem, categória de usuário, Datum, Fenologia, Estágio de Desenvolvimento

---
+++ PortalLab3/src/br/ueg/portalLab/view/composer/EstagioDesenvolvimentoComposer.java
@@ -0,0 +1,24 @@
+package br.ueg.portalLab.view.composer;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import br.ueg.builderSoft.view.zk.composer.TabelaBasicaComposerController;
+import br.ueg.portalLab.model.EstagioDesenvolvimento;
+
+@Component
+@Scope("desktop")
+public class EstagioDesenvolvimentoComposer extends TabelaBasicaComposerController<EstagioDesenvolvimento> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7013854473654426346L;
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Class getEntityClass() {
+		return EstagioDesenvolvimento.class;
+	}
+
+}
