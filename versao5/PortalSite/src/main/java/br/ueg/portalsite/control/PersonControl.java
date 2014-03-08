56e84abadc025fa18ddb6f2bb82da682d262c00c
pvieiraguimaraes@gmail.com <pvieiraguimaraes@gmail.com@cac3254f-852a-ee89-7979-6c55c4adcd7b>
2014-03-08 22:40:33 +0000

    Cruzadinhas adds

--- versao5/PortalSite/src/main/java/br/ueg/portalsite/control/PersonControl.java
+++ versao5/PortalSite/src/main/java/br/ueg/portalsite/control/PersonControl.java
@@ -10,16 +10,17 @@ import br.ueg.portalsite.SiteControl;
 @SuppressWarnings("rawtypes")
 public class PersonControl extends SiteControl<IntegranteEquipe> {
 
+	@SuppressWarnings("unchecked")
 	public List<IntegranteEquipe> getTeam() {
 		IntegranteEquipe integranteEquipe = new IntegranteEquipe<>();
 		List<IntegranteEquipe> list = getListEntity(integranteEquipe);
-		Collections.sort (list, new Comparator() {  
-            public int compare(Object o1, Object o2) {  
-            	IntegranteEquipe p1 = (IntegranteEquipe) o1;  
-            	IntegranteEquipe p2 = (IntegranteEquipe) o2;  
-                return p1.getNome().compareToIgnoreCase(p2.getNome());  
-            }
-        });
+		Collections.sort(list, new Comparator() {
+			public int compare(Object o1, Object o2) {
+				IntegranteEquipe p1 = (IntegranteEquipe) o1;
+				IntegranteEquipe p2 = (IntegranteEquipe) o2;
+				return p1.getNome().compareToIgnoreCase(p2.getNome());
+			}
+		});
 		return list;
 	}
 }
