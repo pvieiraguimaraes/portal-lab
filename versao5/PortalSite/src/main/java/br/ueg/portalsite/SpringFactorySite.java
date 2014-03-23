f15105997085568a1ad6298ab10a9938e77bc5bc
pvieiraguimaraes <pvieiraguimaraes@gmail.com>
2014-03-23 17:11:51 +0000

    Jogos mantidos no portal sendo exibidos no portal

--- versao5/PortalSite/src/main/java/br/ueg/portalsite/SpringFactorySite.java
+++
@@ -1,7 +0,0 @@
-package br.ueg.portalsite;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class SpringFactorySite extends ClassPathXmlApplicationContext {
-	
-}
